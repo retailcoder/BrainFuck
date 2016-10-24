b164b85c3743ad51f12ef602aab96d6631de655a
Mathieu Guindon <retailcoder@gmail.com>
2016-10-24 00:03:33 -0400

    fixed fizzbuzz test

--- BrainFuck.Tests/ExecutionContextTests.cs
+++ BrainFuck.Tests/ExecutionContextTests.cs
@@ -60,6 +60,7 @@ namespace BrainFuck.Tests
         public void ExecutesCodeReviewPost57382()
         {
             // "fizzbuzz in brainfuck"
+            #region code and output strings
             var code = @"
 ++++++++++[>++++++++++<-]>>++++++++++>->>>>>>>>>>>>>>>>-->+++++++[->++
 ++++++++ <]>[->+> +> +> +<<<<]++ +>> +++>>> ++++++++[-< ++++< ++++< ++++>>>]++++
@@ -73,6 +74,109 @@ namespace BrainFuck.Tests
 +++++<] >.[-]] << ++++++[-< ++++++++>] <.[-] <<[-< +>] +[-< +]->>] +[-] <<<.>>> +[
 -< +] -<<]
 ";
+            var output = @"1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+16
+17
+Fizz
+19
+Buzz
+Fizz
+22
+23
+Fizz
+Buzz
+26
+Fizz
+28
+29
+FizzBuzz
+31
+32
+Fizz
+34
+Buzz
+Fizz
+37
+38
+Fizz
+Buzz
+41
+Fizz
+43
+44
+FizzBuzz
+46
+47
+Fizz
+49
+Buzz
+Fizz
+52
+53
+Fizz
+Buzz
+56
+Fizz
+58
+59
+FizzBuzz
+61
+62
+Fizz
+64
+Buzz
+Fizz
+67
+68
+Fizz
+Buzz
+71
+Fizz
+73
+74
+FizzBuzz
+76
+77
+Fizz
+79
+Buzz
+Fizz
+82
+83
+Fizz
+Buzz
+86
+Fizz
+88
+89
+FizzBuzz
+91
+92
+Fizz
+94
+Buzz
+Fizz
+97
+98
+Fizz
+Buzz
+".Replace("\r",string.Empty);
+#endregion
+
             var lexer = new Lexer();
             var tokens = lexer.Tokenize(code).ToArray();
             var parser = new Parser();
@@ -82,7 +186,7 @@ namespace BrainFuck.Tests
             var interpreter = new Interpreter(context);
             interpreter.Execute(tree);
 
-            Assert.AreNotEqual(string.Empty, context.StdOut);
+            Assert.AreEqual(output, context.StdOut);
         }
     }
 }
