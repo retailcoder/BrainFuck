0601d6ba3697e6a392a3fff893c2fc06873f4af5
Mathieu Guindon <retailcoder@gmail.com>
2016-10-24 23:35:27 -0400

    cleaned up exceptions

--- BrainFuck/IllegalInstructionException.cs
+++ BrainFuck/IllegalInstructionException.cs
@@ -15,25 +15,4 @@ namespace BrainFuck
         public Token Token { get; }
         public int Position { get; }
     }
-
-    public abstract class IllegalMoveInstructionException : IllegalInstructionException
-    {
-        protected IllegalMoveInstructionException(string message, Token token, int position)
-            : base(message, token, position)
-        {
-        }
-    }
-
-    public class IllegalMoveLeftInstructionException : IllegalMoveInstructionException
-    {
-        public IllegalMoveLeftInstructionException(Token token, int position) 
-            : base("Instruction moved pointer before index 0.", token, position) { }
-    }
-
-    public class IllegalMoveRightInstructionException : IllegalMoveInstructionException
-    {
-        public IllegalMoveRightInstructionException(Token token, int position) 
-            : base("Instruction moved pointer past the last available index.", token, position) { }
-    }
-
 }
